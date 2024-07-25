32e7b40d0268c687d1af4effb33fa22bbf8c0b6b
M-Aitisam <aitisamahmed24@gmail.com>
2024-07-25 13:00:27 +0500

    Add project files.

---
+++ WebApp/Models/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApp.Models
+{
+    public class ProductDto
+    { 
+      
+        public string Name { get; set; } = "";
+
+        public string Brand { get; set; } = "";
+
+        public string Category { get; set; } = " ";
+        
+        public decimal Price { get; set; } 
+        
+        public string Description { get; set; } = " ";
+
+    }
+}
